460c8b7c88192b0f32d187f59f619a15a26d7b1b
Donald W. Boulton <donaldboulton@gmail.com>
2016-05-02 12:27:27 -0500

    Update Grofile.cs
    Re-named

--- Gravatar/Grofile.cs
+++ Gravatar/Grofile.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Xml.Linq;
 using System.Collections.Generic;
 
-namespace DBAmeretti.Gravatar {
+namespace DBAmaretti.Gravatar {
 
     internal interface IGrofileHelper {
         XDocument LoadXml(string uri);
